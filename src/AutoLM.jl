95bc31f78b759e2be910354cca1a290641f8fd2d
imi-diener <imi.diener@gmail.com>
2020-09-09 10:50:00 +0200

    added main functions for exporting

--- src/AutoLM.jl
+++ src/AutoLM.jl
@@ -6,5 +6,39 @@ include("handling_and_utility.jl")
 include("models.jl")
 include("outliers.jl")
 # Write your package code here.
+export
+    #data augmentation
+    flip_volume_side,
+    flip_volume_front,
+    mirror_vol,
+    flip_3D,
+    jitter_3D,
+    rotate_images,
+    rotate_volumes,
+
+    #utility and data preparation
+    swap_xy,
+    train_test_split_3d,
+    regular_train_test_split_3d,
+    regular_train_test_split_2d,
+    landmark_to_surface,
+    image_gradients,
+    align_principal,
+    resize_relevant,
+    translate_lms_back,
+    choose_dims,
+    depth_map_all_sides,
+
+    #evaluation and training
+    cost_whole_data_2D,
+    cost_whole_data_3D
+    avg_accuracy_per_point,
+    predict_single,
+    predict_set,
+
+    #confidence testing and exporting
+    save_vols_to_folder,
+    response_distribution,
+    procrustes_distance_list
 
 end
